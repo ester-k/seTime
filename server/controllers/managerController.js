e5241dd4b7c89fa3db58af2d291cf134e4e59436
Ester Polack <stp70766@gmail.com>
2020-11-29 15:28:02 +0200

    complete add client function

--- server/controllers/managerController.js
+++ server/controllers/managerController.js
@@ -3,7 +3,7 @@ const managerService = require("../services/managerService");
 const addClient = async (req, res) => {
   try {
     const client = req.body;
-    const addClient = await projectService.addClient(client);
+    const addClient = await managerService.addClient(client);
     return res.status(200).json(addClient);
   } catch (err) {
     return res.status(500).send(err);
--- server/services/managerService.js
+++ server/services/managerService.js
@@ -1,11 +1,12 @@
 const { Client } = require("../models/client");
 const addClient = async (client) => {
   try {
-      console.log("in manager service");
     const createdClient = await Client.create(client);
-    console.log("Client created" + client);
     return createdClient;
   } catch (error) {
     console.log(error);
   }
 };
+module.exports = {
+    addClient,
+}
