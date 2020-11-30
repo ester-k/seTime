efac8402884155d70027b8b2077f5c82c0c4c5e4
Ester <esti24440@gmail.com>
2020-11-30 14:50:17 +0200

    nothing

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const projectController = require('../controllers/projectController');
 router.post('/addProject', projectController.addProject);
 router.get('/getProjects', projectController.getProjects);
-router.get('/getProjectKey/:name', projectController.getProjectKey);
 router.get('/checkProjectName/:name',projectController.checkProjectName);
 router.get('/getSubprojectList/:projectId',projectController.getSubprojectList);
 router.get('/getProjectIdByName/:projectName',projectController.getProjectIdByName);
